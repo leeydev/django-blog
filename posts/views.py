70d317c26656b96c7b5a4c16a56aae57f505fa53
leeydev <adaylete@gmail.com>
2026-04-29 06:05:09 +0000

    Add: forms, templates, admin config, tests

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published=True)
+    posts = Post.objects.filter(published=True).order_by('-created_at')
     return render(request, 'posts/list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk, published=True)
     return render(request, 'posts/detail.html', {'post': post})
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            messages.success(request, 'Post created!')
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'posts/form.html', {'form': form})
+
+@login_required
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk, author=request.user)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'posts/form.html', {'form': form})
